f01a3c26ba023f302601b6b230d8fcd7374f6f3a
yinpeng <2291314224@qq.com>
2017-10-07 16:02:44 +0800

    springboot用jdbcTemplates访问mysql

---
+++ springboot-jdbc/src/main/resources/account.sql
@@ -0,0 +1,14 @@
+-- create table `account`
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `id`    INT(11)     NOT NULL AUTO_INCREMENT,
+  `name`  VARCHAR(20) NOT NULL,
+  `money` DOUBLE               DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 4
+  DEFAULT CHARSET = utf8;
+INSERT INTO `account` VALUES ('1', 'aaa', '1000');
+INSERT INTO `account` VALUES ('2', 'bbb', '1000');
+INSERT INTO `account` VALUES ('3', 'ccc', '1000');
